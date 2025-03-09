c9534838de5831c8ea0936b3c9ff2633c6282986
SandroMaglione <lass.maglio@gmail.com>
2025-03-09 06:06:46 +0100

    `schema` definition

--- packages/schema/src/schema.ts
+++ packages/schema/src/schema.ts
@@ -1,29 +1,28 @@
 import { Schema } from "effect";
 import { LoroDoc } from "loro-crdt";
+import { VERSION } from "./main";
 import { type Version } from "./versioning";
 
 export const AnyLoroDocSchema = Schema.instanceOf(LoroDoc);
 
-export class ActivityV1 extends Schema.Class<ActivityV1>("ActivityV1")({
+export class FoodV1 extends Schema.Class<FoodV1>("FoodV1")({
   id: Schema.UUID,
   name: Schema.String,
+  calories: Schema.Number.pipe(Schema.positive()),
 }) {}
 
-export class ActivityV2 extends Schema.Class<ActivityV2>("ActivityV2")({
+export class MealV1 extends Schema.Class<MealV1>("MealV1")({
   id: Schema.UUID,
-  firstName: Schema.String,
-  lastName: Schema.String,
+  foodId: FoodV1.fields.id,
+  quantity: Schema.Number.pipe(Schema.positive()),
 }) {}
 
-export class ActivityV3 extends Schema.Class<ActivityV3>("ActivityV3")({
-  id: Schema.UUID,
-  firstName: Schema.String,
-  lastName: Schema.String,
-  age: Schema.UndefinedOr(Schema.Number),
-}) {}
-
-export const ActivitySchema = {
-  1: ActivityV1,
-  2: ActivityV2,
-  3: ActivityV3,
+export const Table = Schema.Literal("food", "meal");
+const VersioningSchema = {
+  1: Schema.Struct({
+    [Table.literals[0]]: Schema.Array(FoodV1),
+    [Table.literals[1]]: Schema.Array(MealV1),
+  }),
 } as const satisfies Record<Version, Schema.Schema.AnyNoContext>;
+
+export const CurrentSchema = VersioningSchema[VERSION];
