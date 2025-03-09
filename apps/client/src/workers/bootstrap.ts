edaf82d538b72e0acbe74ca046da22002c8ccd7d
SandroMaglione <lass.maglio@gmail.com>
2025-03-09 05:25:11 +0100

    workers from lib

--- apps/client/src/workers/bootstrap.ts
+++ apps/client/src/workers/bootstrap.ts
@@ -1,42 +1,14 @@
 import { WorkerRunner } from "@effect/platform";
 import { BrowserWorkerRunner } from "@effect/platform-browser";
-import { RuntimeLib, Service, SyncWorker } from "@local/client-lib";
+import { RuntimeLib, SyncWorker } from "@local/client-lib";
 import { Effect, Layer } from "effect";
 
 const WorkerLive = WorkerRunner.layerSerialized(SyncWorker.WorkerMessage, {
   Bootstrap: (params) =>
     Effect.gen(function* () {
-      const { push, pull } = yield* Service.Sync;
-
-      const manager = yield* Service.WorkspaceManager;
-      const temp = yield* Service.TempWorkspace;
-
-      yield* Effect.log(`Running workspace '${params.workspaceId}'`);
-
-      const workspace = yield* manager
-        .getById({ workspaceId: params.workspaceId })
-        .pipe(Effect.flatMap(Effect.fromNullable));
-
-      const tempUpdates = yield* temp.getById({
-        workspaceId: workspace.workspaceId,
-      });
-
-      if (tempUpdates !== undefined) {
-        yield* push({
-          workspaceId: workspace.workspaceId,
-          snapshot: tempUpdates.snapshot,
-          snapshotId: tempUpdates.snapshotId,
-        });
-        yield* Effect.log("Push sync completed");
-      } else {
-        yield* pull({ workspaceId: workspace.workspaceId });
-        yield* Effect.log("Pull sync completed");
-      }
-
-      return true;
-    }).pipe(
-      Effect.mapError((error) => `Bootstrap error: ${JSON.stringify(error)}`)
-    ),
+      const worker = yield* SyncWorker.SyncWorker;
+      return yield* worker.bootstrap(params);
+    }),
 }).pipe(Layer.provide(BrowserWorkerRunner.layer));
 
 RuntimeLib.runFork(WorkerRunner.launch(WorkerLive));
--- apps/client/src/workers/live.ts
+++ apps/client/src/workers/live.ts
@@ -7,15 +7,9 @@ const WorkerLive = WorkerRunner.layer((params: SyncWorker.LiveQuery) =>
   Effect.scoped(
     Effect.gen(function* () {
       const worker = yield* SyncWorker.SyncWorker;
-      yield* Effect.log("Startup live query connection");
-
-      yield* Effect.addFinalizer(() =>
-        Effect.log("Closed live query connection")
-      );
-
       yield* Effect.fork(worker.liveSync({ workspaceId: params.workspaceId }));
       yield* Effect.never;
-    }).pipe(Effect.mapError(() => "Live query error"))
+    })
   )
 ).pipe(Layer.provide(BrowserWorkerRunner.layer));
 
